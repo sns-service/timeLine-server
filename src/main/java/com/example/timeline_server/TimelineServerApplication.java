674094648c42cf487e5654b623b07fd82eaaaa5f
matt <matt1235@naver.com>
2024-05-25 15:28:08 +0900

    Initial Commit

---
+++ src/main/java/com/example/timeline_server/TimelineServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.timeline_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimelineServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TimelineServerApplication.class, args);
+	}
+
+}
